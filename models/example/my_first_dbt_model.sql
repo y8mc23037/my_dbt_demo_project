49be29730d4cacfed8c16f08ec17dc8a478dca32
kalla.ramakrishna@gmail.com <kalla.ramakrishna@gmail.com>
2025-07-27 12:17:16 +0000

    first Program

---
+++ models/example/my_first_dbt_model.sql
@@ -0,0 +1,15 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+select
+  current_date as run_date,
+  current_user as user
+
